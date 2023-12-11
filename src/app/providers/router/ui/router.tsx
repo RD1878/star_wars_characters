7c08ea4d8d42d13ed856849fc9b2b2a1bdd33bf1
Pavel Khokhlov <pkhokhlov@naumen.ru>
2023-12-11 23:16:46 +0300

    add HashRrouter

--- src/app/providers/router/ui/router.tsx
+++ src/app/providers/router/ui/router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { CharactersPage } from 'src/pages/CharactersPage';
 import { CharacterPage } from 'src/pages/CharacterPage';
 import { routes } from 'src/shared/routes/routes';
