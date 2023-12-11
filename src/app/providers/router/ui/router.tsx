00bab08bc7bf8abe97117fbe55aae04c0f833d95
Pavel Khokhlov <pkhokhlov@naumen.ru>
2023-12-11 23:29:25 +0300

    fix HashRrouter

--- src/app/providers/router/ui/router.tsx
+++ src/app/providers/router/ui/router.tsx
@@ -5,7 +5,7 @@ import { CharacterPage } from 'src/pages/CharacterPage';
 import { routes } from 'src/shared/routes/routes';
 
 const AppRouter = () => (
-  <Router basename="/star_wars_characters">
+  <Router basename="/">
     <Routes>
       <Route path={routes.home} element={<CharactersPage />} />
       <Route path={`${routes.character}:id`} element={<CharacterPage />} />
